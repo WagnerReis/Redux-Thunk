927ba6db89641131ebe61100d14d3cbf6aeec3a4
Wagner Reis <wagnerreis1997@gmail.com>
2020-02-11 14:00:15 -0300

    Preparando o projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import logger from 'redux-logger'
+
+import { reducer } from './reducer'
+import Info from './Info'
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div>
+        <Info />
+      </div>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
